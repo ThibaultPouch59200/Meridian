0b68d09a4c623f7688b5c0174b32b7bafb69305b
Thibault Pouch <thibault.pouch@epitech.eu>
2026-06-07 14:08:04 +0200

    feat(store): initialize matrix store on app startup

--- app/plugins/stores.client.ts
+++ app/plugins/stores.client.ts
@@ -1,9 +1,12 @@
 import { useEventsStore } from '~/stores/events'
 import { useTagsStore } from '~/stores/tags'
+import { useMatrixStore } from '~/stores/matrix'
 
 export default defineNuxtPlugin(() => {
   const eventsStore = useEventsStore()
   const tagsStore = useTagsStore()
+  const matrixStore = useMatrixStore()
   eventsStore.load()
   tagsStore.load()
+  matrixStore.load()
 })
