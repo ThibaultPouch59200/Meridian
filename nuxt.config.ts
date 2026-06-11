9818c2af35491415e311be8f02b0f460a248d5f8
Thibault Pouch <thibault.pouch@epitech.eu>
2026-06-11 23:42:21 +0200

    style(favicon): add SVG favicon with serif M

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,8 @@ export default defineNuxtConfig({
     head: {
       meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
       link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Instrument+Sans:wght@300;400;500;600&display=swap',
