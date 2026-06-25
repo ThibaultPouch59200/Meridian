825d28238ddf408b17f87baf5c055e4ffa70ffd6
Thibault Pouch <thibault.pouch@epitech.eu>
2026-06-25 14:58:51 +0200

    feat(store): wire stores to API on startup and update MatrixQuadrant for async actions

--- app/plugins/stores.client.ts
+++ app/plugins/stores.client.ts
@@ -2,11 +2,13 @@ import { useEventsStore } from '~/stores/events'
 import { useTagsStore } from '~/stores/tags'
 import { useMatrixStore } from '~/stores/matrix'
 
-export default defineNuxtPlugin(() => {
+export default defineNuxtPlugin(async () => {
   const eventsStore = useEventsStore()
   const tagsStore = useTagsStore()
   const matrixStore = useMatrixStore()
-  eventsStore.load()
   tagsStore.load()
-  matrixStore.load()
+  await Promise.all([
+    eventsStore.fetch(),
+    matrixStore.fetch(),
+  ])
 })
