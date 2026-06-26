811c3c8f8638104a91ee11a3dfe791293b044bb1
Thibault Pouch <thibault.pouch@epitech.eu>
2026-06-26 11:17:27 +0200

    style(dark-theme): add dark backgrounds to layouts

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex w-full h-screen overflow-hidden bg-gray-50">
+  <div class="flex w-full h-screen overflow-hidden bg-gray-50 dark:bg-[#111111]">
     <AppSidebar />
-    <div class="flex-1 min-w-0 sm:border-l border-gray-200 bg-white flex flex-col h-screen overflow-hidden pb-14 sm:pb-0">
+    <div class="flex-1 min-w-0 sm:border-l border-gray-200 dark:border-[#2e2e2e] bg-white dark:bg-[#1a1a1a] flex flex-col h-screen overflow-hidden pb-14 sm:pb-0 text-black dark:text-[#f0f0ee]">
       <slot />
     </div>
   </div>
